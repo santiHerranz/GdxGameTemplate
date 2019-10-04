ea34b4ece7dfbc5a32f52cd8118bb95196b46c97
Santi Herranz <santi.herranz@gmail.com>
2019-10-04 20:38:01 +0200

    first commit

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Game";
+		config.width = 800;
+		config.height = 600;
+		config.resizable = false;
+
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
